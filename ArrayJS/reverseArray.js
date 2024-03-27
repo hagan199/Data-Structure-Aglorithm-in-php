d63e28f4b8e444985a3e6e9936c0d2d36260558c
Emmanuel Hagan <32996656+hagan199@users.noreply.github.com>
2024-03-27 17:07:53 +0000

    Adding Comment

--- ArrayJS/reverseArray.js
+++ ArrayJS/reverseArray.js
@@ -1,13 +1,16 @@
 /* Solution 1
 1. Given string - convert it into array.
 2. reverse it the array.
-3. turn the array back i.
+3. turn the array back .
 */
 const reverseString = function (string) {
   const arr = string.split("");
-  arr.reverse();
+  const re = arr.reverse();
+  console.log('Test1', re);
+  const countArray = arr.length; // Count the elements in the array
+  console.log('countArray', countArray);
   string = arr.join("");
-  console.log('Test', string);
+  console.log('Test2', string);
 };
 reverseString("Greating");
 
@@ -22,3 +25,13 @@ const reverseString2 = function (string) {
   console.log('Test 2',reversed);
 };
 reverseString2("Greating");
+
+/*Test1 [
+  'g', 'n', 'i',
+  't', 'a', 'e',
+  'r', 'G'
+]
+Test3 8
+Test gnitaerG
+Test 2 gnitaerG
+*/
